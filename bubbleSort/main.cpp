3a063b1cf05edbf4e1c4de6c8a1ed02918325ff2
roaming <wilhelmus4321@gmail.com>
2025-09-25 20:02:13 +0100

    CodeWithMosh excersise bubbleSort

---
+++ bubbleSort/main.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<int> S4 = {4, 3, 2, 1, 2, 4, 9, 42, 3, 7};
+
+    for (int j = 0; j < S4.size() -1 ; j++) 
+    {
+        for (int i = 0; i < S4.size() - 1; i++)
+        {
+                if (S4.at(i) >= S4.at(i+1))
+                {
+                    std::swap(S4[i], S4.at(i+1));
+                }
+        }
+    }
+    
+    for (int i = 0; i < S4.size(); i++)
+    {
+        std::cout << S4.at(i) << " ";
+    }
+    return 0;
+}
